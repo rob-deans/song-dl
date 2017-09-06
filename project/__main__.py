31d22c7c3d68bc3939d5840afc3549f955f548d5
Rob Deans <robertdeans@ntlworld.com>
2017-09-06 22:58:10 +0100

    Added pushbullet notification support

--- project/__main__.py
+++ project/__main__.py
@@ -2,9 +2,36 @@ from __future__ import unicode_literals
 import youtube_dl
 import sys
 import argparse
+import requests
+import json
+
+def my_hook(d):
+
+	if d['status'] == 'finished':
+		print 'Finished downloading'
+		song_name=d['filename'][:-17]
+		print song_name
+		payload={
+			'title': 'Downloaded',
+			'body': song_name,
+		}
+		
+		headers={
+			'Access-Token': '',
+			'Content-Type': 'application/json'
+		}
+
+		r = requests.post(
+			'https://api.pushbullet.com/v2/pushes',
+			data=json.dumps(payload),
+			headers=headers
+		)
+
+		print r.text
+
 
 def main(args):
-	ydl_opts = {
+	ydl_opts={
 		'format': 'bestaudio/best',
 		'ignoreerrors': True,
 		'continue': True,
@@ -14,6 +41,7 @@ def main(args):
 			'preferredcodec': 'mp3',
 			'preferredquality': '192'
 		}],
+		'progress_hooks': [my_hook]
 	
 	} # Pass in arguments for this
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -21,10 +49,9 @@ def main(args):
 
 if __name__ == '__main__':
 	# Get all the options from the config file
-	parser = argparse.ArgumentParser()
+	parser=argparse.ArgumentParser()
 	parser.add_argument(
 		'yt_link', help = 'the youtube link to download'
 	)
-	args = parser.parse_args()
-	print args.yt_link
+	args=parser.parse_args()
 	main(args.yt_link)
